c422fa0cf87b9b7a75078ae6b55139a83f69fc7b
Sarah Nowotny <snowotny47@gmail.com>
2022-02-11 09:13:06 -0600

    test commit

--- array-list-practice/src/ArrayListPractice.java
+++ array-list-practice/src/ArrayListPractice.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListPractice {
     public static void main(String[] args) throws Exception {
-        // Add Array List methods from the worksheet here
         
+    }
+    public void removeEvenLength(List myList){
+        for(int i =0; i<=((CharSequence) myList).length(); i++){
+            if (((CharSequence) myList.get(i)).length()%2 == 0){
+                myList.remove(i);
+            }
+        }
+    }
+    public void averageVowels(){
+
+    }
+    public void removeDuplicates(){
+
+    }
+    public void doubleList(){
+
+    }
+    public void minToFront(){
+
     }
 }
